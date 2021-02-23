5a8d6eed40d0b74b053e9ec5e148bd057045c4b6
Adrián <github@adrianpaniagua.es>
2021-02-24 00:26:35 +0100

    Version 1

---
+++ bot.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import telebot
+import requests,json
+from telebot import types
+from flask import Flask, request
+import os
+from datetime import datetime
+
+
+
+TOKEN = os.environ['TOKEN']
+WEBHOOK = os.environ['WEBHOOK']
+
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+
+@bot.message_handler(commands=['programacion'])
+def eventos(message):
+
+	url = "https://www.marca.com/programacion-tv.html"
+	cid = message.chat.id
+
+	r = requests.get(url)
+	resp= r.text
+
+	evento = {}
+	hora = {}
+	canal = {}
+	titulo = {}
+	deporte = {}
+	emoji = {}
+
+
+	hoy = resp.split('<span class="title-section-widget"><strong>')[1].split('</span>')[0]
+	hoy = hoy.replace("</strong>", " ")
+	proximos_eventos = resp.split('<span class="title-section-widget"><strong>')[1].split('<span class="title-section-widget"><strong>')[0]
+	eventos_hoy = proximos_eventos.count('<li class="dailyevent">')
+	print(eventos_hoy)
+
+	mensaje = "["+hoy+"]\n\n\n"
+
+	for i in range (eventos_hoy):
+		i = i+1
+		evento[i] = proximos_eventos.split('<li class="dailyevent">')[i].split('</li>')[0]
+		hora[i] = evento[i].split('<strong class="dailyhour">')[1].split('</strong>')[0]
+		titulo[i] = evento[i].split('<a href="" title="')[1].split('">')[0]
+		canal [i] = evento[i].split('<span class="dailychannel"><i class="icon-pantalla"></i>')[1].split('</span>')[0]
+		deporte[i] = evento[i].split('<i class="')[1].split('"></i>')[0]
+		if (deporte[i] == "icon-futbol" ):
+			emoji[i] = "⚽"
+		elif (deporte[i] == "icon-baloncesto" ):
+			emoji[i]= "🏀"
+		elif (deporte[i] == "icon-ciclismo" ):
+			emoji[i] = "🚴‍♂"
+		elif (deporte[i] == "icon-atletismo" ):
+			emoji[i] = "🏃‍♀️"
+		elif (deporte[i] == "icon-golf" ):
+			emoji[i] = "🏌️‍♀️"
+		elif (deporte[i] == "icon-rugby" ):
+			emoji[i] = "🏉"
+		elif (deporte[i] == "icon-boxeo" ):
+			emoji[i] = "🥊"
+		elif (deporte[i] == "icon-tenis" ):
+			emoji[i] = "🎾"
+		elif (deporte[i] == "icon-balonmano" ):
+			emoji[i] = "🤾"
+		else:
+			emoji[i] = "🏆"
+
+		mensaje += emoji[i]+" "+titulo[i]+" - "+hora[i]+" - "+canal[i]+"\n\n"
+
+
+	bot.send_message(cid,mensaje)
+	bot.delete_message(message.chat.id, message.message_id)
+
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+	bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+	return "!", 200
+
+
+bot.polling()
